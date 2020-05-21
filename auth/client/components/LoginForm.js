ba28c62f1ef9031eb2a9a8516c0c9d5581bb5cb7
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-21 16:45:02 -0600

    auth
    login mutation

--- auth/client/components/LoginForm.js
+++ auth/client/components/LoginForm.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Login';
 
 import AuthForm from './AuthForm';
 
 const LoginForm = () => {
   return (
     <div className="container">
+      <h3>Login</h3>
       <AuthForm />
     </div>
   );
 };
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
