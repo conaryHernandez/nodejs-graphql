21668c55fb6181702ea86d6f3808d2bdc6db7e19
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-21 21:28:42 -0600

    auth
    error handling

--- auth/client/components/AuthForm.js
+++ auth/client/components/AuthForm.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
 
-const AuthFornm = () => {
+const AuthFornm = ({ onSubmit, errors }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    onSubmit({ email, password });
+  };
+
   return (
     <div className="row">
-      <form className="col-s4">
+      <form className="col-s4" onSubmit={onSubmitHandler}>
         <div className="input-field">
-          <label>Email</label>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} />
+          <input
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
 
         <div className="input-field">
-          <label>Password</label>
           <input
+            placeholder="Password"
+            type="password"
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
+        </div>
+
+        <div className="errors">
+          {errors.map((error) => (
+            <div key={error}>{error}</div>
+          ))}
         </div>
 
         <button className="btn">Submit</button>
--- auth/client/components/LoginForm.js
+++ auth/client/components/LoginForm.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
-import mutation from '../mutations/Login';
-
 import AuthForm from './AuthForm';
 
-const LoginForm = () => {
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
+
+const LoginForm = ({ mutate }) => {
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = ({ email, password }) => {
+    mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }],
+    }).catch((res) => {
+      const errors = res.graphQLErrors.map((error) => error.message);
+
+      setErrors(errors);
+    });
+  };
+
   return (
     <div className="container">
       <h3>Login</h3>
-      <AuthForm />
+      <AuthForm errors={errors} onSubmit={onSubmit} />
     </div>
   );
 };
