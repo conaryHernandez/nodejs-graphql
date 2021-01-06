ba08c35c2963d1dea799a622cd6df30476ac161a
conaryHernandez <conaryh@gmail.com>
2021-01-06 16:29:11 -0600

    playground
    adding uuid
    createUser mutation
    createPost mutation
    createComment mutation

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,23 +1,24 @@
 import { GraphQLServer } from 'graphql-yoga';
+import uuidv4 from 'uuid/v4';
 
 // Scalar Types  = String, Boolean, Int, Float, ID
 
 // Demo Data
 const users = [
   {
-    id: 1,
+    id: '1',
     name: 'Conary',
     email: 'Conary@example.com',
     age: 25,
   },
   {
-    id: 2,
+    id: '2',
     name: 'Sara',
     email: 'Sara@example.com',
     age: 30,
   },
   {
-    id: 3,
+    id: '3',
     name: 'John',
     email: 'John@example.com',
     age: 20,
@@ -26,25 +27,25 @@ const users = [
 
 const posts = [
   {
-    id: 1,
+    id: '1',
     title: 'First Post',
     body: 'this is a post 1',
     published: 25,
-    author: 1,
+    author: '1',
   },
   {
-    id: 2,
+    id: '2',
     title: 'second Post',
     body: 'this is a post 2',
-    apublishedge: 30,
-    author: 1,
+    published: 30,
+    author: '1',
   },
   {
-    id: 3,
+    id: '3',
     title: 'third Post',
     body: 'this is a post 3',
     published: 20,
-    author: 2,
+    author: '2',
   },
 ];
 
@@ -52,25 +53,25 @@ const comments = [
   {
     id: 'comment-1',
     text: 'First Comment',
-    author: 1,
+    author: '1',
     post: 1,
   },
   {
     id: 'comment-2',
     text: 'second Comment',
-    author: 1,
+    author: '1',
     post: 1,
   },
   {
     id: 'comment-3',
     text: 'third Comment',
-    author: 3,
+    author: '3',
     post: 1,
   },
   {
     id: 'comment-4',
     text: 'forth Comment',
-    author: 3,
+    author: '3',
     post: 1,
   },
 ];
@@ -83,6 +84,12 @@ const typeDefs = `
     comments: [Comment!]!
     me: User!
     post: Post!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
   
   type User {
@@ -153,6 +160,71 @@ const resolvers = {
         body: "I'm writing my first post",
         published: true,
       };
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.email);
+
+      if (emailTaken) {
+        throw new Error('Email Taken.');
+      }
+
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        age: args.age,
+      };
+
+      users.push(user);
+
+      return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+
+      if (!userExists) {
+        throw new Error('Invalid User.');
+      }
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+
+      if (!userExists) {
+        throw new Error('Invalid User.');
+      }
+
+      const postExists = posts.some(
+        (post) => post.id === args.post && post.published
+      );
+
+      if (!postExists) {
+        throw new Error('Invalid Post.');
+      }
+
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        post: args.post,
+        author: args.author,
+      };
+
+      comments.push(comment);
+
+      return comment;
     },
   },
   Post: {
